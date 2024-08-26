0f5778e56d0614e0bafc49537089ced12356c559
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2024-08-26 15:08:30 +0500

    Implemented init, setContrast functions

--- tiny1106.cpp
+++ tiny1106.cpp
@@ -1,1 +1,23 @@
 #include <tiny1106.h>
+
+Oled::Oled(uint8_t address)
+{
+    _address = address;
+}
+
+void Oled::init()
+{
+    Wire.beginTransmission(_address);
+    Wire.write(OLED_ONE_COMMAND_MODE);
+    Wire.write(0xAF); // Display on
+    Wire.endTransmission();
+}
+
+void Oled::setContrast(uint8_t contrast)
+{
+    Wire.beginTransmission(_address);
+    Wire.write(OLED_COMMAND_MODE);
+    Wire.write(OLED_CONTRAST);
+    Wire.write(contrast);
+    Wire.endTransmission();
+}
--- tiny1106.h
+++ tiny1106.h
@@ -1,7 +1,40 @@
 #ifndef tiny1106_h
 #define tiny1106_h
+
 #include <Arduino.h>
+#include <Wire.h>
 
+#include <charMapEN.h>
 
+#ifdef OLED_RU
+#include <charMapRU.h>
+#endif
+
+#define OLED_COMMAND_MODE 0x00
+#define OLED_ONE_COMMAND_MODE 0x80
+#define OLED_DATA_MODE 0x40
+#define OLED_ONE_DATA_MODE 0xC0
+
+#define OLED_CONTRAST 0x81
+
+class Oled
+{
+public:
+    Oled(uint8_t address = 0x3C);
+    void init();
+    void setContrast(uint8_t contrast);
+    void clear();
+    void clear(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+
+    void drawPoint(uint8_t x, uint8_t y);
+    void drawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+
+    void print(uint8_t x, uint8_t y, char text[]);
+
+    void setTextScale(uint8_t scale);    
+
+private:
+    uint8_t _address;
+};
 
 #endif
