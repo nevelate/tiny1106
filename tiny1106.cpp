805b10a4c32f40258b61e276b5ee9804972b0c54
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2024-08-27 15:51:34 +0500

    Implemented clear, drawPoint, drawLine(V/H)

--- tiny1106.cpp
+++ tiny1106.cpp
@@ -1,3 +1,6 @@
+// Credits:
+// http://www.technoblogy.com/show?23OS
+
 #include <tiny1106.h>
 
 Oled::Oled(uint8_t address)
@@ -5,14 +8,16 @@ Oled::Oled(uint8_t address)
     _address = address;
 }
 
+// Initialize display
 void Oled::init()
 {
     Wire.beginTransmission(_address);
-    Wire.write(OLED_ONE_COMMAND_MODE);
-    Wire.write(0xAF); // Display on
+    sendOneCommand(OLED_DISPLAY_ON);
+    setContrast(255);
     Wire.endTransmission();
 }
 
+// Set display contrast (0-255)
 void Oled::setContrast(uint8_t contrast)
 {
     Wire.beginTransmission(_address);
@@ -20,4 +25,177 @@ void Oled::setContrast(uint8_t contrast)
     Wire.write(OLED_CONTRAST);
     Wire.write(contrast);
     Wire.endTransmission();
+}
+
+void Oled::clear()
+{
+    for (uint8_t p = 0; p < 8; p++)
+    {
+        Wire.beginTransmission(_address);
+        Wire.write(OLED_PAGE + p);
+        Wire.endTransmission();
+
+        for (uint8_t i = 0; i < 8; i++)
+        {
+            Wire.beginTransmission(_address);
+            Wire.write(OLED_DATA_MODE);
+            for (int i = 0; i < 17; i++)
+                Wire.write(0);
+            Wire.endTransmission();
+        }
+    }
+}
+
+void Oled::clear(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1)
+{
+}
+
+void Oled::drawPoint(uint8_t x, uint8_t y)
+{
+    Wire.beginTransmission(_address);
+    Wire.write(OLED_COMMAND_MODE);
+
+    Wire.write(OLED_PAGE + (y >> 3));
+    Wire.write(OLED_COLUMN_LOWER_BITS + ((x + OLED_OFFSET) & 0x0F)); // may not work
+    Wire.write(OLED_COLUMN_HIGHER_BITS + ((x + OLED_OFFSET) >> 4));
+
+    Wire.write(OLED_READ_MODIFY_WRITE);
+    Wire.endTransmission();
+
+    Wire.requestFrom(_address, 2);
+    Wire.read();
+    int8_t data = Wire.read();
+    Wire.beginTransmission(_address);
+    Wire.write(OLED_ONE_DATA_MODE);
+    Wire.write((1 << (y & 0x07)) | data);
+
+    sendOneCommand(OLED_END);
+
+    Wire.endTransmission();
+}
+
+void Oled::drawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1)
+{
+    int sx, sy, e2, err;
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    if (x0 < x1)
+        sx = 1;
+    else
+        sx = -1;
+    if (y0 < y1)
+        sy = 1;
+    else
+        sy = -1;
+    err = dx - dy;
+    for (;;)
+    {
+        drawPoint(x0, y0);
+        if (x0 == x1 && y0 == y1)
+            return;
+        e2 = err << 1;
+        if (e2 > -dy)
+        {
+            err = err - dy;
+            x0 = x0 + sx;
+        }
+        if (e2 < dx)
+        {
+            err = err + dx;
+            y0 = y0 + sy;
+        }
+    }
+}
+
+void Oled::drawLineV(uint8_t x, uint8_t y0, uint8_t y1)
+{
+    uint8_t start = y1 > y0 ? y0 : y1;
+    uint8_t end = y1 > y0 ? y1 : y0;
+
+    for (uint8_t i = (start >> 3); i <= (end >> 3); i++)
+    {
+        Wire.beginTransmission(_address);
+        Wire.write(OLED_COMMAND_MODE);
+
+        Wire.write(OLED_PAGE + i);
+        Wire.write(OLED_COLUMN_LOWER_BITS + ((x + OLED_OFFSET) & 0x0F)); // may not work
+        Wire.write(OLED_COLUMN_HIGHER_BITS + ((x + OLED_OFFSET) >> 4));
+        Wire.endTransmission();
+
+        if (i == (start >> 3))
+        {
+            Wire.requestFrom(_address, 2);
+            Wire.read();
+            int8_t data = Wire.read();
+            Wire.beginTransmission(_address);
+            Wire.write(OLED_ONE_DATA_MODE);
+            Wire.write((0xFF >> (start & 0x07)) | data);
+
+            sendOneCommand(OLED_END);
+
+            Wire.endTransmission();
+        }
+        else if (i == (end >> 3))
+        {
+            Wire.requestFrom(_address, 2);
+            Wire.read();
+            int8_t data = Wire.read();
+            Wire.beginTransmission(_address);
+            Wire.write(OLED_ONE_DATA_MODE);
+            Wire.write(~(0xFF >> (end & 0x07)) | data);
+
+            sendOneCommand(OLED_END);
+
+            Wire.endTransmission();
+        }
+        else
+        {
+            Wire.beginTransmission(_address);
+            Wire.write(OLED_ONE_DATA_MODE);
+            Wire.write(0xFF);
+            Wire.endTransmission();
+        }
+    }
+}
+
+void Oled::drawLineH(uint8_t y, uint8_t x0, uint8_t x1)
+{
+    uint8_t x = x0;
+    Wire.beginTransmission(_address);
+    sendOneCommand(OLED_PAGE + (y >> 3));
+    Wire.endTransmission();
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        Wire.beginTransmission(_address);
+        Wire.write(OLED_COLUMN_LOWER_BITS + ((x + OLED_OFFSET) & 0x0F)); // may not work
+        Wire.write(OLED_COLUMN_HIGHER_BITS + ((x + OLED_OFFSET) >> 4));
+        Wire.endTransmission();
+
+        for (uint8_t j = 0; j < 17; j++)
+        {
+            Wire.beginTransmission(_address);
+            sendOneCommand(OLED_READ_MODIFY_WRITE);
+            Wire.endTransmission();
+
+            Wire.requestFrom(_address, 2);
+            Wire.read();
+            int8_t data = Wire.read();
+            Wire.beginTransmission(_address);
+            Wire.write(OLED_ONE_DATA_MODE);
+            Wire.write((1 << (y & 0x07)) | data);
+            sendOneCommand(OLED_END);
+            Wire.endTransmission();
+
+            if (x == x1)
+                return;
+            x++;
+        }
+    }
+}
+
+void Oled::sendOneCommand(int8_t command)
+{
+    Wire.write(OLED_ONE_COMMAND_MODE);
+    Wire.write(command);
 }
--- tiny1106.h
+++ tiny1106.h
@@ -1,14 +1,24 @@
+// Credits:
+// http://www.technoblogy.com/show?23OS
+
 #ifndef tiny1106_h
 #define tiny1106_h
 
 #include <Arduino.h>
 #include <Wire.h>
 
-#include <charMapEN.h>
+#include <charMap.h>
 
 #ifdef OLED_RU
 #include <charMapRU.h>
 #endif
+
+#ifndef OLED_OFFSET
+#define OLED_OFFSET 2
+#endif
+
+#define OLED_DISPLAY_ON 0xAF
+#define OLED_DISPLAY_OFF 0xAE
 
 #define OLED_COMMAND_MODE 0x00
 #define OLED_ONE_COMMAND_MODE 0x80
@@ -16,6 +26,16 @@
 #define OLED_ONE_DATA_MODE 0xC0
 
 #define OLED_CONTRAST 0x81
+
+#define OLED_NORMAL 0xA6
+#define OLED_INVERTED 0xA7
+
+#define OLED_READ_MODIFY_WRITE 0xE0
+#define OLED_END 0xEE
+
+#define OLED_PAGE 0xB0
+#define OLED_COLUMN_HIGHER_BITS 0x10
+#define OLED_COLUMN_LOWER_BITS 0x00
 
 class Oled
 {
@@ -27,14 +47,19 @@ public:
     void clear(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
 
     void drawPoint(uint8_t x, uint8_t y);
+
     void drawLine(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+    void drawLineV(uint8_t x, uint8_t y0, uint8_t y1);
+    void drawLineH(uint8_t y, uint8_t x0, uint8_t x1);
 
     void print(uint8_t x, uint8_t y, char text[]);
+    void printChar(uint8_t x, uint8_t y, char character);
 
-    void setTextScale(uint8_t scale);    
+    void setTextScale(uint8_t scale);
 
 private:
     uint8_t _address;
+    void sendOneCommand(int8_t command);
 };
 
 #endif
